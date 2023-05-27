8002e60679d9eb4dc3881c86d2eb39adf92e3dfd
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-05-27 15:11:39 +0800

    feat: 357 use loading/error state

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,15 +1,24 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
 
 function UsersList() {
   const dispatch = useDispatch();
+  const { data, isLoading, error } = useSelector((state) => state.users);
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]); // 只有在第一次載入元件會執行，所以 dependencies 為空就好，但 eslint..
 
-  return <h1>UsersList</h1>;
+  if (isLoading) {
+    return <div>is loading...</div>;
+  }
+
+  if (error) {
+    return <div>error fetching data...</div>;
+  }
+
+  return <h1>{data.length}</h1>;
 }
 
 export default UsersList;
