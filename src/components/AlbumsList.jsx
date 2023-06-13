f03e3519d13a1c509888c85b96ad784447532fbb
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-06-13 10:40:24 +0800

    feat: 387 - 397 add album on click

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,13 +1,19 @@
-import { useFetchAlbumsQuery } from '../store';
+import { useFetchAlbumsQuery, useAddAlbumMutation } from '../store';
 import Skeleton from './Skeleton';
 import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
 
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
 
   let content;
   if (isLoading) {
-    content = <Skeleton times={3} />;
+    content = <Skeleton className="h-10 w-full" times={3} />;
   } else if (error) {
     content = <div>Error loading albums..</div>;
   } else {
@@ -22,11 +28,14 @@ function AlbumsList({ user }) {
     });
   }
 
-  console.log(content);
-
   return (
     <div>
-      <div>Albums for {user.name}</div>
+      <div className="m-2 flex items-center justify-between">
+        <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+        <Button onClick={handleAddAlbum} loading={results.isLoading}>
+          + Add Album
+        </Button>
+      </div>
       <div>{content}</div>
     </div>
   );
--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,13 +1,29 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { faker } from '@faker-js/faker';
+
+// !!! DEV ONLY
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export const albumsApi = createApi({
   reducerPath: 'albums',
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:3005',
+    // !!! DEV ONLY
+    fetchFn: async (...args) => {
+      await pause(1000);
+      return fetch(...args);
+    },
   }),
   endpoints(builder) {
     return {
       fetchAlbums: builder.query({
+        providesTags: (result, error, user) => {
+          return [{ type: 'Album', id: user.id }];
+        }, // 呼叫的 mutation 有標記這個 tag，就會觸發 fetchAlbums
         query: (user) => {
           return {
             url: '/albums',
@@ -18,8 +34,23 @@ export const albumsApi = createApi({
           };
         },
       }),
+      addAlbum: builder.mutation({
+        invalidatesTags: (result, error, user) => {
+          return [{ type: 'Album', id: user.id }];
+        }, // So now whenever we run this mutation, we're going to go and find all the queries that make use of this tag. Mark them as out of date. Those queries are going to be executed again
+        query: (user) => {
+          return {
+            url: '/albums',
+            method: 'POST',
+            body: {
+              userId: user.id,
+              title: faker.commerce.productName(),
+            },
+          };
+        },
+      }),
     };
   },
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
